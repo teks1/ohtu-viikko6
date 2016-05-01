b901f6758852aad7713afa7f14171dc7f54ac318
teks1 <marko.vuorenmaa@cs.helsinki.fi>
2016-05-01 20:52:50 +0300

    Kuudes viikko, teht 4

--- QueryLanguage1/src/main/java/statistics/Main.java
+++ QueryLanguage1/src/main/java/statistics/Main.java
@@ -6,24 +6,47 @@ public class Main {
 
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
+        QueryBuilder query = new QueryBuilder();
 
-        Matcher m = new And(new HasAtLeast(10, "goals"),
-                new HasAtLeast(10, "assists"),
-                new PlaysIn("PHI")
-        );
+        Matcher m = query.playsIn("NYR")
+                .hasAtLeast(10, "goals")
+                .hasFewerThan(25, "assists").build();
 
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
-        System.out.println("------");
-        Matcher m2 = new Not(new Or(new HasFewerThan(1, "goals"),
-                new PlaysIn("PHI"),
-                new And(new HasAtLeast(10, "goals"),
-                        new HasAtLeast(10, "assists"),
-                        new PlaysIn("PHI"))
-        ));
 
+        System.out.println("--------");
+        Matcher m1 = query.playsIn("PHI")
+                .hasAtLeast(10, "goals")
+                .hasFewerThan(20, "assists").build();
+        for (Player player : stats.matches(m1)) {
+            System.out.println(player);
+        }
+
+        System.out.println("--------");
+        Matcher m2 = query.playsIn("EDM")
+                .hasAtLeast(50, "points").build();
         for (Player player : stats.matches(m2)) {
+            System.out.println(player);
+        }
+
+        System.out.println("--------");
+        Matcher m3 = query.oneOf(
+                query.playsIn("PHI")
+                .hasAtLeast(10, "goals")
+                .hasFewerThan(15, "assists").build(),
+                query.playsIn("EDM")
+                .hasAtLeast(50, "points").build()
+        ).build();
+
+        for (Player player : stats.matches(m3)) {
+            System.out.println(player);
+        }
+
+        System.out.println("--------");
+        Matcher m4 = query.oneOf(m1, m2).build();
+        for (Player player : stats.matches(m3)) {
             System.out.println(player);
         }
     }
--- QueryLanguage1/src/main/java/statistics/QueryBuilder.java
+++ QueryLanguage1/src/main/java/statistics/QueryBuilder.java
@@ -1,21 +1,60 @@
-
 package statistics;
 
+import statistics.matcher.And;
+import statistics.matcher.HasAtLeast;
+import statistics.matcher.HasFewerThan;
 import statistics.matcher.Matcher;
-
+import statistics.matcher.Not;
+import statistics.matcher.Or;
+import statistics.matcher.PlaysIn;
 
 public class QueryBuilder {
+
     Matcher matcher;
-    
-//    public QueryBuilder(){
-//        matcher = new Matcher() {
-//
-//            @Override
-//            public boolean matches(Player p) {
-//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//            }
-//        }
-//    }
-    
-    
+
+    public QueryBuilder() {
+        this.init();
+    }
+
+    private void init() {
+        this.matcher = new Matcher() {
+
+            @Override
+            public boolean matches(Player p) {
+                return true;
+            }
+        };
+    }
+
+    public Matcher build() {
+        Matcher m = this.matcher;
+        this.init();
+        return m;
+    }
+
+    QueryBuilder playsIn(String team) {
+        this.matcher = new And(new PlaysIn(team), matcher);
+        return this;
+    }
+
+    QueryBuilder hasAtLeast(int value, String category) {
+        this.matcher = new And(new HasAtLeast(value, category), matcher);
+        return this;
+    }
+
+    QueryBuilder hasFewerThan(int value, String category) {
+        this.matcher = new And(new HasFewerThan(value, category), matcher);
+        return this;
+    }
+
+    QueryBuilder not(Matcher matcher) {
+        this.matcher = new Not(matcher);
+        return this;
+    }
+
+    QueryBuilder oneOf(Matcher... matchers) {
+        this.matcher = new Or(matchers);
+        return this;
+    }
+
 }
